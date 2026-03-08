9d1606e98cb37bac5aced2d25dd9021c4d760d8a
Syandilya <93141167+Syandilya@users.noreply.github.com>
2026-03-08 10:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (76.95%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,31 +1,8 @@
-
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        Set<String> sets = new HashSet<>(Arrays.asList(nums));
-        String[] dp = new String[nums.length + 1];
-        Arrays.fill(dp, "");
-        return help(nums, dp, nums.length, "", sets);
-    }
-
-    private String help(String[] nums, String[] dp, int i, String s, Set<String> sets) {
-        if (i < 0) {
-            return s;
-        }
-        if (i == 0) {
-            return sets.contains(s) ? "" : s;
-        }
-        if (!dp[i].isEmpty()) {
-            return dp[i];
-        }
-        
-        String op1 = s + "0";
-        dp[i] = help(nums, dp, i - 1, op1, sets);
-        if (!dp[i].isEmpty()) {
-            return dp[i];
-        }
-
-        String op2 = s + "1";
-        dp[i] = help(nums, dp, i - 1, op2, sets);
-        return dp[i];
+        StringBuilder ans= new StringBuilder();                  
+        for(int i=0; i<nums.length; i++)  
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');              // Using ternary operator
+        return ans.toString();
     }
 }
